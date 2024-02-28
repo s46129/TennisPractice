99022ca59f4f1754f0a5f4fdfd465dc95b5c99b3
Rex <s46129@yahoo.com.tw>
2024-02-28 21:52:30 +0800

    feat : add 20240228 practice

---
+++ TennisPractice20240228/TennisPractice20240228/Tennis.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace TennisPractice20240228
+{
+    public class Tennis
+    {
+        private int _firstPlayerScore;
+
+        public string FistPlayerScore()
+        {
+            var scoreLookup = new Dictionary<int, string>
+            {
+                { 1, "Fifteen" },
+                { 2, "Thirty" },
+                { 3, "Forty" }
+            };
+            if (_firstPlayerScore > 0) return $"{scoreLookup[_firstPlayerScore]} Love";
+            return "Love All";
+        }
+
+        public void FirstPlayerScore()
+        {
+            _firstPlayerScore++;
+        }
+    }
+}
---
+++ TennisPractice20240228/TennisPractice20240228/TennisPractice.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace TennisPractice20240228
+{
+    public class TennisPractice
+    {
+        private Tennis _tennis;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tennis = new Tennis();
+        }
+
+        [Test]
+        public void Love_All()
+        {
+            ScoreShouldBe("Love All");
+        }
+
+        [Test]
+        public void Fifteen_Love()
+        {
+            GivenFirstPlayerScore(1);
+            ScoreShouldBe("Fifteen Love");
+        }
+
+        [Test]
+        public void Thirty_Love()
+        {
+            GivenFirstPlayerScore(2);
+            ScoreShouldBe("Thirty Love");
+        }
+
+        [Test]
+        public void Forty_Love()
+        {
+            GivenFirstPlayerScore(3);
+            ScoreShouldBe("Forty Love");
+        }
+
+        private void GivenFirstPlayerScore(int times)
+        {
+            for (var i = 0; i < times; i++) _tennis.FirstPlayerScore();
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, _tennis.FistPlayerScore());
+        }
+    }
+}
